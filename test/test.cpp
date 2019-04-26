fb1a65ed7dc3a80c247cb4b73cd6b7837fffaad3
AnnaTuzova <anntuz00@gmail.com>
2019-04-26 13:06:41 +0300

    v2_1

--- test/test.cpp
+++ test/test.cpp
@@ -6,28 +6,28 @@ TEST(TestCalculator, OperationsTest)
 {
 	Calculator calculator;
 	std::vector<std::string> input_strings{"1 + 1", "5 - 9", "8 * 6", "15/4", "-(-5)", "6^2"};
-	std::vector<float> actual_results{ 2, -4, 48, 3.75, 5, 36 };
-	std::vector<float> expected_results(actual_results.size());
+	std::vector<float> expected_results{ 2, -4, 48, 3.75, 5, 36 };
+	std::vector<float> actual_results(expected_results.size());
 
 	for (int i = 0; i < input_strings.size(); i++)
 	{
-		expected_results.at(i) = calculator.RunCalculator(input_strings.at(i));
+		actual_results.at(i) = calculator.RunCalculator(input_strings.at(i));
 	}
-		ASSERT_EQ(expected_results, actual_results);
+		ASSERT_EQ(actual_results, expected_results);
 }
 
 TEST(TestCalculator, ModAndSqrtTest)
 {
 	Calculator calculator;
 	std::vector<std::string> input_strings{ "sqrt(16)", "mod(5,2)" };
-	std::vector<float> actual_results{ 4, 1 };
-	std::vector<float> expected_results(actual_results.size());
+	std::vector<float> expected_results{ 4, 1 };
+	std::vector<float> actual_results(expected_results.size());
 
 	for (int i = 0; i < input_strings.size(); i++)
 	{
-		expected_results.at(i) = calculator.RunCalculator(input_strings.at(i));
+		actual_results.at(i) = calculator.RunCalculator(input_strings.at(i));
 	}
-	ASSERT_EQ(expected_results, actual_results);
+	ASSERT_EQ(actual_results, expected_results);
 }
 
 
@@ -35,14 +35,14 @@ TEST(TestCalculator, MWAndMRTest)
 {
 	Calculator calculator;
 	std::vector<std::string> input_strings{ "1 + 5", "MW", "MR", "6 + 5", "MW[1]", "MR + MR[1] + 1" };
-	std::vector<float> actual_results{ 6, 6, 6, 11, 11, 18 };
-	std::vector<float> expected_results(actual_results.size());
+	std::vector<float> expected_results{ 6, 6, 6, 11, 11, 18 };
+	std::vector<float> actual_results(expected_results.size());
 
 	for (int i = 0; i < input_strings.size(); i++)
 	{
-		expected_results.at(i) = calculator.RunCalculator(input_strings.at(i));
+		actual_results.at(i) = calculator.RunCalculator(input_strings.at(i));
 	}
-	ASSERT_EQ(expected_results, actual_results);
+	ASSERT_EQ(actual_results, expected_results);
 }
 
 TEST(TestCalculator, ExpressionTest)
@@ -50,13 +50,13 @@ TEST(TestCalculator, ExpressionTest)
 	Calculator calculator;
 	std::vector<std::string> input_strings{ "((5.2^2+45.6)*(58.6+99.001))^(0.5)-98/17.2---89-+mod(--55,1)*sqrt(sqrt(18^2))",
 					"------8+-+-+-+-9*sqrt(2568)*(mod(mod(156,56.5),45.4)-  99/4.1)="};
-	std::vector<float> actual_results{ 12.2983f, 8606.7509f };
-	std::vector<float> expected_results(actual_results.size());
+	std::vector<float> expected_results{ 12.2983f, 8606.7509f };
+	std::vector<float> actual_results(expected_results.size());
 
 	for (int i = 0; i < input_strings.size(); i++)
 	{
-		expected_results.at(i) = calculator.RunCalculator(input_strings.at(i));
-		ASSERT_NEAR(expected_results.at(i), actual_results.at(i), 1e-4);
+		actual_results.at(i) = calculator.RunCalculator(input_strings.at(i));
+		ASSERT_NEAR(actual_results.at(i), expected_results.at(i), 1e-4);
 	}
 	
 }
